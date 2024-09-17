fe93d4856466329d8ecd584c9cfeab64512f4331
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-17 08:37:43 -0300

    Confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -13,10 +13,17 @@ function changeImageButtonStatus(game){
         image.classList.remove('dashboard__item__img--rented');
         button.classList.remove('dashboard__item__button--return');
         button.textContent = 'Alugar';
+        let confirmation = prompt('Digite CONFIRMAR para que a devolução seja concluída');
+
+        if (confirmation == 'CONFIRMAR' || confirmation == 'confirmar'){
+            alert('Item devolvido');
+        } else {
+            alert('Digite a palavra correta para devolver');
+            confirmation = prompt('Digite CONFIRMAR para que a devolução seja concluída');
+        }
     } else {
         image.classList.add('dashboard__item__img--rented');
         button.classList.add('dashboard__item__button--return');
         button.textContent = 'Devolver';
     }   
 }
-
